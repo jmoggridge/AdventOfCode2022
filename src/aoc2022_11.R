99cefca8c3cad04bf8cd326d40c0f37826e87f05
jmoggridge <ja.moggridge@gmail.com>
2022-12-12 19:16:32 -0800

    finally remembered how to use modulus

---
+++ src/aoc2022_11.R
@@ -0,0 +1,99 @@
+library(tidyverse)
+
+file <- 'input/input-11.txt'
+# file <- 'input/test.txt'
+
+notes <-
+  readr::read_file(file) |>
+  str_split(pattern = '\n\n') |>
+  unlist() |>
+  map(~str_split(.x, '\n') |> unlist()) |>
+  map_dfr(~{
+    lst(
+      monkey   = parse_number(.[1]) + 1,
+      items    = str_extract_all(.[2], '[0-9]+') |> map(as.numeric),
+      worry    = str_remove(.[3], 'Operation: new =') |> str_trim(),
+      test     = parse_number(.[4]),
+      if_true  = parse_number(.[5]) + 1,
+      if_false = parse_number(.[6]) + 1,
+    )
+  }) |>
+  group_by(monkey) |>
+  nest(behaviour = c(worry, test, if_true, if_false))
+
+do_rounds <- function(rounds, worry_reduction = 3) {
+  monkey_counter <- rep(0, nrow(notes))
+
+  keepaway <- function(notes, i, worry_reduction){
+    monkey <- notes$behaviour[[i]]
+    for (old in notes$items[[i]]) {
+      # log inspection in global scope
+      monkey_counter[[i]] <<- monkey_counter[[i]] + 1
+      # item: inspect, test, toss
+      new <- (eval(parse(text = monkey$worry)) / worry_reduction) %/% 1
+      test_rs <- new %% monkey$test == 0
+      if (test_rs) {
+        notes$items[[monkey$if_true]] <- c(notes$items[[monkey$if_true]], new)
+      } else {
+        notes$items[[monkey$if_false]] <- c(notes$items[[monkey$if_false]], new)
+      }
+    }
+    notes$items[[i]] <- numeric(length = 0L)
+    return(notes)
+  }
+
+  for (round in 1:20) {
+    for (monkey in seq(1:nrow(notes))) {
+      notes <- keepaway(notes, monkey, worry_reduction)
+    }
+  }
+  return(monkey_counter)
+}
+
+do_rounds(rounds = 20, worry_reduction = 3) |> sort(decreasing = T) |> head(2) |> prod()
+
+
+
+
+
+
+monkey_business <- function(notes, rounds) {
+
+  keepaway <- function(notes, i, modular_size){
+    monkey <- notes$behaviour[[i]]
+    for (old in notes$items[[i]]) {
+      monkey_counter[[i]] <<- monkey_counter[[i]] + 1
+      new <- (eval(parse(text = monkey$worry)) |> round(0)) %% modular_size
+      test_rs <- new %% monkey$test == 0L
+      if (test_rs) {
+        notes$items[[monkey$if_true]] <- c(notes$items[[monkey$if_true]], new)
+      } else {
+        notes$items[[monkey$if_false]] <- c(notes$items[[monkey$if_false]], new)
+      }
+    }
+    notes$items[[i]] <- numeric(length = 0L)
+    return(notes)
+  }
+
+  modular_size <- notes |>
+    unnest(behaviour) |>
+    pull(test) |>
+    prod()
+
+  monkey_counter <- rep(0, nrow(notes))
+
+  for (round in 1:rounds) {
+    for (monkey in seq(1:nrow(notes))) {
+      notes <- keepaway(notes, monkey, modular_size)
+    }
+  }
+  return(monkey_counter)
+}
+
+monkey_business(notes, rounds = 10000) |>
+  sort(decreasing = T) |>
+  head(2) |>
+  prod() |>
+  print()
+
+
